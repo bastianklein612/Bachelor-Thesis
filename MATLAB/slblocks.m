7936baa292ea87787c3a0887a951befaf32cec0b
Bastian Klein <bastianklein612@gmail.com>
2023-08-09 12:02:20 +0200

    Started to create a custom library for frequently used blocks

---
+++ MATLAB/slblocks.m
@@ -0,0 +1,7 @@
+function blkStruct = slblocks
+% This function specifies that the library 'Custom_Library'
+% should appear in the Library Browser with the 
+% name 'Custom Hexapod Blocks'
+    Browser.Library = 'Custom_Library';
+    Browser.Name = 'Custom Hexapod Blocks';
+    blkStruct.Browser = Browser;
